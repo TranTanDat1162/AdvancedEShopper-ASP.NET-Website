3b3d35e6559cc369ae88c3de77d01ba74924c01d
TranTanDat1162 <dattt11062002@gmail.com>
2023-11-04 10:53:50 +0700

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdvancedEShopper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
